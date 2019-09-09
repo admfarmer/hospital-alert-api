6c3d514ec8f7a1dad4ceba945be4f3dc93a656e4
thawatchai <thawatchai.sea2@gmail.com>
2019-09-09 14:11:56 +0700

    not stattus

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -277,12 +277,12 @@ const router = (fastify, { }, next) => {
         // console.log(messages);
         const rs_191ubon: any = botlineModel.botLineToken(messages, token191ubon);
       });
-      const itemUpdate: any = await statusModel.getUpdate(db);
-      console.log(itemUpdate);
+      // const itemUpdate: any = await statusModel.getUpdate(db);
+      // console.log(itemUpdate);
 
     }
 
-    // const itemUpdate: any = await statusModel.getUpdate(db);
+    const itemUpdate: any = await statusModel.getUpdate(db);
     // console.log(itemUpdate);
 
   });
